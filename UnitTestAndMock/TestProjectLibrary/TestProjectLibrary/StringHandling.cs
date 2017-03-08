1790d589e8c5794ac6c8c954e5030731fdb17cd6
yangchangzhi <690674930@qq.com>
2017-03-08 16:21:49 +0800

    unit test practice

---
+++ UnitTestAndMock/TestProjectLibrary/TestProjectLibrary/StringHandling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestProjectLibrary
+{
+    public class StringHandling
+    {
+        public string Contact(string name, string surname)
+        {
+            return $"{name}{surname}";
+        }
+
+        public string InitalAndCleanUp()
+        {
+            return "InitalAndCleanUp";
+        }
+
+        public double Divide(double first, double second)
+        {
+            if (second.Equals(0))
+            {
+               throw  new DivideByZeroException();
+            }
+            return first/second;
+        }
+
+    }
+}
---
+++ UnitTestAndMock/TestProjectLibrary/UnitTestProject/UnitTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TestProjectLibrary;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        //[TestMethod]
+        //public void TestContact()
+        //{
+        //    //Arange
+        //    StringHandling handling= new StringHandling();
+        //    //Act
+        //    var contactStr = handling.Contact("Hello", "World");
+        //    //Assert
+        //    Assert.AreEqual("HelloWorld",contactStr);
+
+        //    //第一个值和第二个值之间不超过50就通过，超过了就不通过
+        //    //Assert.AreEqual(100,200,delta:50);
+        //}
+
+        #region TestCleanUp and TestInitialize
+
+        private StringHandling _obj;
+        private string _value;
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _obj = null;
+            _value = string.Empty;
+        }
+
+        [TestInitialize]
+        public void InitalAndCleanUp()
+        {
+            _obj= new StringHandling();
+            _value = _obj.InitalAndCleanUp();
+        }
+        [TestMethod]
+        public void InitalAndCleanUpTest1()
+        {
+            Assert.AreEqual(_value,_obj.InitalAndCleanUp());
+        }
+
+        [TestMethod]
+        public void InitalAndCleanUpTest2()
+        {
+            Assert.AreEqual(_value, _obj.InitalAndCleanUp());
+        }
+
+        #endregion
+
+        #region CollectionAssert
+
+        [TestMethod]
+        public void CollectionAssertTest()
+        {
+            List<string> first = new List<string>();
+            first.Add("a");
+
+            List<string> second = new List<string>();
+            second.Add("a");
+
+            CollectionAssert.AreEqual(first,second);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivideTest()
+        {
+            var result = _obj.Divide(10, 0);
+            Assert.AreEqual(10,result);
+        }
+
+
+        #endregion
+
+        [TestMethod]
+        public void MockTest()
+        {
+            Mock<CheckEmployee> mock = new Mock<CheckEmployee>();
+            mock.Setup(x => x.CheckEmp()).Returns(false);
+            ProcessEmployee process= new ProcessEmployee();
+            Assert.AreEqual(process.InsertEmployee(mock.Object), true);
+        }
+    }
+
+    public class CheckEmployee
+    {
+        public virtual bool CheckEmp()
+        {
+            throw  new NotImplementedException();
+        }
+    }
+
+    public class ProcessEmployee
+    {
+        public bool InsertEmployee(CheckEmployee checkEmployee)
+        {
+            checkEmployee.CheckEmp();
+            return true;
+        }
+    }
+}
