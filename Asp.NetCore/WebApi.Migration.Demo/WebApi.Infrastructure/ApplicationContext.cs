fe67f800fc6fbc286522efb69675c0e70c064e7a
yangchangzhi <690674930@qq.com>
2017-06-02 09:41:55 +0800

    add dbcontext libary

---
+++ Asp.NetCore/WebApi.Migration.Demo/WebApi.Infrastructure/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebApi.Infrastructure.Models;
+
+namespace WebApi.Infrastructure
+{
+    public class ApplicationContext : DbContext
+    {
+ 
+        public ApplicationContext(DbContextOptions options) : base(options) {
+           
+        }
+
+        public DbSet<Student> Student { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Initial Catalog=WebApi.Migration.Demo.Lib;User Id=sa;Password=cmsWL2016...;MultipleActiveResultSets=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
